3f50dc2dc3f8ddc1312c5949395b6a293d85b5a2
DJ <dhruv.jain9100@gmail.com>
2025-07-17 13:28:34 +0530

    Add project files.

---
+++ BaseLibrary/DTOs/Register.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseLibrary.DTOs
+{
+    public class Register : AccountBase
+    {
+        [Required(ErrorMessage = "Full Name is required")]
+        [MinLength(5)]
+        [MaxLength(100)]
+        public string? FullName { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "Passwords do not match.")]
+        [Required(ErrorMessage = "Confirm Password is required.")]
+        public string? ConfirmPassword { get; set; }
+    }
+}
---
+++ BaseLibrary/Entities/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseLibrary.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public string? CivilId { get; set; }
+
+        public string? FileNumber { get; set; }
+
+        public string? FullName { get; set; }
+
+        public string? JobName { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? TelephoneNumber { get; set; }
+
+        public string? Photo { get; set; }
+
+        public string? Other { get; set; }
+    }
+}
